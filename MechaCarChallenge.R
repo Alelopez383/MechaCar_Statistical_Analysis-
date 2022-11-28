65d11e525933a5cad0d9d49b9e4cc9b7e8ad0d94
Alelopez383 <43974872+Alelopez383@users.noreply.github.com>
2022-11-28 00:05:35 -0600

    adding summary tables

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -31,3 +31,20 @@ lm(mpg ~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD
 
 #generate summary statistics
 summary(lm(mpg ~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_table))
+
+
+#Deliverable 2: Create Visualizations for the Trip Analysis
+suspension_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+?summarize()
+total_summary <- suspension_table %>% summarize(Mean=mean(PSI),
+                                                Median=median(PSI),
+                                                Var=var(PSI),
+                                                Std_Dev=sd(PSI),
+                                                .groups = 'keep')
+
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+                                                                         Median=median(PSI),
+                                                                         Var=var(PSI),
+                                                                         Std_Dev=sd(PSI),
+                                                                         .groups = 'keep')  
